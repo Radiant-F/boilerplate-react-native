3da6240d312b614e031cc7b801beedfff36fcda0
Radiant-F <radiantfadilah0@gmail.com>
2022-06-07 15:55:12 +0700

    refactor

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,15 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Auth, Dashboard} from '../screens/exports';
+import {NavigationContainer} from '@react-navigation/native';
+import {Auth, Dashboard} from '../screens/export';
 
 const Stack = createNativeStackNavigator();
-
 export default function Navigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Auth" component={Auth} />
+      <Stack.Navigator initialRouteName="Auth">
         <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="Auth" component={Auth} />
       </Stack.Navigator>
     </NavigationContainer>
   );

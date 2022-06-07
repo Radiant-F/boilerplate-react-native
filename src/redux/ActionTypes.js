f7697f55c388bd1d9b7ddb39939468ab8e34d0f1
Radiant-F <radiantfadilah0@gmail.com>
2022-06-07 15:54:08 +0700

    initial commit

---
+++ src/redux/ActionTypes.js
@@ -0,0 +1,4 @@
+const AUTH_TYPE = 'AUTH_TYPE';
+const USER_DATA = 'USER_DATA';
+
+export {AUTH_TYPE, USER_DATA};
---
+++ src/screens/export.js
@@ -0,0 +1,3 @@
+import Auth from './Auth/Auth';
+import Dashboard from './Dashboard/Dashboard';
+export {Auth, Dashboard};

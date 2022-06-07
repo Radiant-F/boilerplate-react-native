bd70e5b777350911cfeb4e00ba228ede98efb6e0
Radiant-F <radiantfadilah0@gmail.com>
2022-06-07 15:53:33 +0700

    deleted due to unused

--- src/components/exports.js
+++
@@ -1,4 +0,0 @@
-import Login from './screens/auth/Login';
-import Register from './screens/auth/Register';
-
-export {Login, Register};
--- src/screens/exports.js
+++
@@ -1,4 +0,0 @@
-import Auth from './Auth/Auth';
-import Dashboard from './Dashboard/Dashboard';
-
-export {Dashboard, Auth};

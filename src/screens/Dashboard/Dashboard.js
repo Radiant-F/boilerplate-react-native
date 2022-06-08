ddfd97779527c180c1ac0e68e3ca910d982f3baa
Radiant-F <radiantfadilah0@gmail.com>
2022-06-08 14:36:06 +0700

    user selector

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {useSelector} from 'react-redux';
 
 export default function Dashboard() {
+  const {user_data} = useSelector(state => state.auth);
+  console.log(user_data);
   return (
     <View>
       <Text>Dashboard</Text>

92be64466304a45235714afcd9ed03580f7fbc9f
Radiant-F <radiantfadilah0@gmail.com>
2022-06-09 09:57:22 +0700

    missing extrareducer and bruh

--- src excercise/redux/reducer/auth.js
+++ src excercise/redux/reducer/auth.js
@@ -1,26 +1,25 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {AUTH_TYPE, USER_DATA} from '../ActionTypes';
+import {AUTH_TYPE} from '../ActionTypes';
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    is_login: false,
+    is_login: true,
     user_data: null,
     status: null,
   },
   reducers: {
-    authReducer: (state, action) => {
-      const {type, payload} = action.payload;
+    authReducer: (state, payload) => {
+      const {type, payload} = payload;
       switch (type) {
         case AUTH_TYPE:
           return {...state, is_login: !state.is_login};
-        case USER_DATA:
-          return {...state, user_data: payload};
         default:
           return state;
       }
     },
   },
+  extraReducers: {},
 });
 
 export const {authReducer} = authSlice.actions;

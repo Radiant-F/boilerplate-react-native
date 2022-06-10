b7af45524946c2972b98ec9f3830d79eaeca7f5b
Radiant-F <radiantfadilah0@gmail.com>
2022-06-10 09:00:13 +0700

    dispatch and navigate

--- src excercise/redux/actions/auth.js
+++ src excercise/redux/actions/auth.js
@@ -1,9 +1,13 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {fetchLogin} from '../../utils/APIs';
+import {USER_DATA} from '../ActionTypes';
+import {authReducer} from '../reducer/auth';
 
 export const login = createAsyncThunk(
   'auth/login',
   async ({formData, navigation}, {dispatch}) => {
     const {data} = await fetchLogin(formData);
+    dispatch(authReducer({type: USER_DATA, payload: data}));
+    navigation.navigate('Dashboard');
   },
 );

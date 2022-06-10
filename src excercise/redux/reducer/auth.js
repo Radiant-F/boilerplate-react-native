ca13cef157f06eba08513fea1dcd4851309842fb
Radiant-F <radiantfadilah0@gmail.com>
2022-06-10 09:11:50 +0700

    new case, extra reducers

--- src excercise/redux/reducer/auth.js
+++ src excercise/redux/reducer/auth.js
@@ -1,25 +1,38 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {AUTH_TYPE} from '../ActionTypes';
+import {login} from '../actions/auth';
+import {AUTH_TYPE, USER_DATA} from '../ActionTypes';
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    is_login: true,
+    is_login: false,
     user_data: null,
     status: null,
   },
   reducers: {
-    authReducer: (state, payload) => {
-      const {type, payload} = payload;
+    authReducer: (state, action) => {
+      const {type, payload} = action;
       switch (type) {
         case AUTH_TYPE:
           return {...state, is_login: !state.is_login};
+        case USER_DATA:
+          return {...state, user_data: payload};
         default:
           return state;
       }
     },
   },
-  extraReducers: {},
+  extraReducers: {
+    [login.fulfilled]: (state, action) => {
+      state.status = 'success';
+    },
+    [login.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [login.rejected]: (state, action) => {
+      state.status = 'error';
+    },
+  },
 });
 
 export const {authReducer} = authSlice.actions;
